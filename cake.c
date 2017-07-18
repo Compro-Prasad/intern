a18fca3d0b06c10cd50578571e26b2fadc9d3914
Compro Prasad <Compro-Prasad@users.noreply.github.com>
2017-07-18 12:00:49 +0530

    Works for the given test cases

--- cake.c
+++ cake.c
@@ -4,21 +4,29 @@ using namespace std;
 
 int lost_cake(int N, int E, int S, int D, int A[40001], int B[40001])
 {
-    vector<int> arr[40001];
-    for (int i = 0; i < E; ++i) {
-        arr[A[i]].push_back(B[i]);
-        arr[B[i]].push_back(A[i]);
-    }
-    queue<int> q;
-    vector<bool> nodes(0);
-    q.push(s);
-    for (int i = arr[S][0]; !q.empty();) {
-      if (nodes[i] == 0) {
-        i = arr[i][0];
-        nodes[i] = 1;
-        q.insert(i)
+  vector<int> arr[10001];
+  for (int i = 0; i < E; ++i) {
+      arr[A[i]].push_back(B[i]);
+      arr[B[i]].push_back(A[i]);
+  }
+  int path = 0;
+  queue<int> q;
+  vector<bool> nodes(0);
+  q.push(S);
+  for (int i = S; i != D && !q.empty();) {
+    path++;
+    for (vector<int>::iterator j = arr[i].begin(); j != arr[i].end(); ++j) {
+      if (nodes[*j] == 0) {
+        q.push(*j);
+        nodes[*j] = 1;
       }
+      if (*j == D)
+        return path;
     }
+    q.pop();
+    i = q.front();
+  }
+  return path;
 }
 
 int main()
